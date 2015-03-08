3b2fc6f9066af1e17217c3ed92a2fbda3efedeb9
tanyanova <ivashova.t@gmail.com>
2015-03-08 16:11:43 +0400

    new list view, add item in top menu

---
+++ js/application.js
@@ -0,0 +1,31 @@
+$(function () {
+    $(document).ready(function () {
+
+//        $('.slideshow').cycle({
+//            fx: 'carousel',
+//            speed: 500,
+//            slides: '> li',
+////            carouselVisible: 3,
+//            timeout: 0,
+//            prev: '.prev-slide',
+//            next: '.next-slide',
+//            pager: '.slideshow-pager',
+//            allowWrap: false
+//        });
+
+//        $('.slideshow').on('cycle-update-view', function (event, opts) {
+//            var i = $(".slideshow").data("cycle.opts").currSlide;
+//            var c = $(".slideshow").data("cycle.opts").slideCount;
+//            $('.slideshow-num').html(i);
+//            $('.slideshow-count').html(c);
+//        });
+
+
+
+
+    });
+});
+
+
+
+
